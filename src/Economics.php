3b59bd6cf4bb3874146902022d2bf9e21669c26a
Tobias <tobias@osandweb.dk>
2021-12-01 12:27:56 +0100

    Added Carbon and more functionality

--- src/Economics.php
+++ src/Economics.php
@@ -3,13 +3,14 @@
 namespace Tobiasfp\Economics;
 
 use GuzzleHttp\Client;
-use Symfony\Component\Serializer\Serializer;
+use carbon\carbon;
+use Exception;
+
 class Economics
 {
     public string $appToken;
     public string $grant;
     public Client $client;
-
 
     public function __construct(string $appToken, string $grant)
     {
@@ -22,19 +23,65 @@ class Economics
     }
 
 
-    public function customers(): Array
+    public function customers(): array
     {
-        $serializer = new serializer();
-        $customersBody = $this->client->get("customers")->getBody();
-        $customers = json_decode($customersBody)->collection;
-        return $customers;
+        $customersRes = $this->client->get("customers")->getBody();
+        return json_decode($customersRes)->collection;
     }
 
     public function customer(string $id): Object
     {
-        $serializer = new serializer();
-        $customerBody = $this->client->get("customers/".$id)->getBody();
+        $customerBody = $this->client->get("customers/" . $id)->getBody();
         return json_decode($customerBody);
     }
 
+    public function validateCustomerObject(Object $customer): void
+    {
+        $requiredProperties = ["attention", "currency", "customerContact", "customerGroup", "customerGroup", "defaultDeliveryLocation", "invoices", "layout", "name", "paymentTerms", "paymentTerms", "salesPerson", "self", "templates", "totals", "vatZone", "vatZone"];
+        foreach ($requiredProperties as $requiredProp) {
+            if (property_exists($customer, $requiredProp) && $customer->customerNumber < 1) {
+                throw new Exception("Customer is not valid, misses " . $requiredProp);
+            }
+        }
+    }
+
+    public function invoiceDrafts(): array
+    {
+        $invoicesRes = $this->client->get("invoices/drafts")->getBody();
+        return json_decode($invoicesRes)->collection;
+    }
+
+    public function invoiceDraft(string $id): Object
+    {
+        $invoicesRes = $this->client->get("invoices/drafts/" . $id)->getBody();
+        return json_decode($invoicesRes);
+    }
+
+    public function createInvoice(Object $customer, Carbon $date, Object $layout, Object $paymentterms, string $currency = "DKK", Object $recipient = null): Object
+    {
+        if (!$this->validateCurrency($currency)) {
+            throw new Exception("Currency is not allowed");
+        }
+
+        $this->validateCustomerObject($customer);
+
+        return new \stdClass;
+    }
+
+    public function currencies(): array
+    {
+        $currencies = $this->client->get("currencies")->getBody();
+        return json_decode($currencies)->collection;
+    }
+
+    public function validateCurrency(string $currency, bool $validateAgainstApi = false): bool
+    {
+        $currencyCodes = ["AED", "ALL", "ARS", "AUD", "AZN", "BAM", "BDT", "BGN", "BHD", "BND", "BOB", "BRL", "BWP", "BYR", "CAD", "CDF", "CFA", "CHF", "CLP", "CNY", "COP", "CRC", "CYP", "CZK", "DKK", "DZD", "EEK", "EGP", "ETB", "EUR", "GBP", "GEL", "GHS", "GMD", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "IQD", "ISK", "JOD", "JPY", "KES", "KRW", "KWD", "LBP", "LKR", "LTL", "LVL", "MAD", "MDL", "MKD", "MMK", "MTL", "MUR", "MWK", "MXN", "MYR", "MZN", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PEN", "PGK", "PHP", "PKR", "PLN", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SCR", "SDG", "SEK", "SGD", "SIT", "SKK", "SLL", "SSP", "SYP", "THB", "TND", "TRY", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "VEF", "VND", "XCD", "XOF"];
+        if ($validateAgainstApi) {
+            $currencyCodes = array_map(function (Object $currency) {
+                return $currency->code;
+            }, $this->currencies());
+        }
+        return in_array($currency, $currencyCodes);
+    }
 }
--- tests/economicsTest.php
+++ tests/economicsTest.php
@@ -2,22 +2,68 @@
 
 use Tobiasfp\Economics\Economics;
 use PHPUnit\Framework\TestCase;
-
+use Carbon\Carbon;
 
 class economicsTest extends TestCase
 {
     private Economics $econ;
 
-    public function setup(): void {
+    public function setup(): void
+    {
         $this->econ = new Economics("demo", "demo");
     }
 
     public function testCustomers()
     {
-        self::assertEquals($this->econ->customers()[0]->customerNumber, 1);
+        self::assertEquals(1, $this->econ->customers()[0]->customerNumber);
     }
-    
-    public function testGetCustomer() {
-        self::assertEquals($this->econ->customer(1)->customerNumber, 1);
+
+    public function testCustomer()
+    {
+        self::assertEquals(1, $this->econ->customer(1)->customerNumber);
+    }
+
+    public function testInvoiceDrafts()
+    {
+        self::assertEquals(123267, $this->econ->invoiceDrafts()[0]->draftInvoiceNumber);
+    }
+
+
+    public function testInvoiceDraft()
+    {
+        self::assertEquals(123267, $this->econ->invoiceDraft(123267)->draftInvoiceNumber);
+    }
+
+    public function testCreateInvoice()
+    {
+        $customer = $this->econ->customer(1);
+        $date = Carbon::now();
+        $layout = new \stdClass;
+        $paymentTerms = new \stdClass;
+        $this->econ->createInvoice($customer, $date, $layout, $paymentTerms, "DKK");
+    }
+
+    public function testCreateInvoiceWithInvalidCurrency()
+    {
+        $customer = $this->econ->customer(1);
+        $date = Carbon::now();
+        $layout = new \stdClass;
+        $paymentTerms = new \stdClass;
+        $this->expectException(\Exception::class);
+        $this->econ->createInvoice($customer, $date, $layout, $paymentTerms, "asdf");
+    }
+    public function testCurrencies()
+    {
+        $currencies = $this->econ->currencies();
+        self::assertNotEmpty($currencies);
+        self::assertEquals("AED", $currencies[0]->code);
+    }
+
+    public function testValidateCurrency()
+    {
+        self::assertEquals(true, $this->econ->validateCurrency("DKK", true));
+        self::assertEquals(false, $this->econ->validateCurrency("fsafasf", true));
+        self::assertEquals(true, $this->econ->validateCurrency("DKK", false));
+        self::assertEquals(false, $this->econ->validateCurrency("fsafasf", false));
     }
 }
