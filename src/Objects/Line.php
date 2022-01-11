914b6c18aeb7a16dd54ddc8975eed14f3e1cae46
Tobias <tobias@osandweb.dk>
2022-01-11 14:59:00 +0100

    Added unitNetPrice and totalnetamount to line

--- src/Objects/Line.php
+++ src/Objects/Line.php
@@ -9,13 +9,17 @@ class Line
     public int $quantity;
     public object $product;
     public array $unit;
+    public float $unitNetPrice;
+    public float $totalNetAmount;
 
-    public function __construct(object $product, Unit $unit, string $description = '', int $quantity = 1, int $lineNumber = 1)
+    public function __construct(object $product, Unit $unit, string $description = '', int $quantity = 1, float $unitNetPrice = 0, float $totalNetAmount = 0, int $lineNumber = 1)
     {
         $this->description = $description;
         $this->lineNumber = $lineNumber;
         $this->quantity = $quantity;
         $this->product = $product;
         $this->unit = (array)$unit;
+        $this->unitNetPrice = $unitNetPrice;
+        $this->totalNetAmount = $totalNetAmount;
     }
 }
